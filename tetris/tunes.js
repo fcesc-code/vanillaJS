66a3d06b00e3e8f602cb61ebcea5105c375674d3
fcesc-code <bpfran@gmail.com>
2021-01-17 15:55:50 +0100

    updated tunes js

--- tetris/tunes.js
+++ tetris/tunes.js
@@ -44,6 +44,50 @@ const mainTune = [
   [0, 4]
 ]
 
+const mainTune = [
+  [329.6, 4],
+  [246.9, 8],
+  [261.6, 8],
+  [293.6, 4],
+  [261.6, 8],
+  [246.9, 8],
+  [220.0, 4],
+  [220.0, 8],
+  [261.6, 8],
+  [329.6, 4],
+  [293.6, 8],
+  [261.6, 8],
+  [246.9, 4],
+  [246.9, 8],
+  [261.6, 8],
+  [293.6, 4],
+  [329.6, 4],
+  [261.6, 4],
+  [220.0, 4],
+  [220.0, 4],
+  [0, 4],
+  [0, 8],
+  [293.6, 4],
+  [349.2, 8],
+  [440.0, 4],
+  [392.0, 8],
+  [349.2, 8],
+  [329.6, 8/3],
+  [261.6, 8],
+  [329.6, 4],
+  [293.6, 8],
+  [261.6, 8],
+  [246.9, 4],
+  [246.9, 8],
+  [261.6, 8],
+  [293.6, 4],
+  [329.6, 4],
+  [261.6, 4],
+  [220.0, 4],
+  [220.0, 4],
+  [0, 4]
+]
+
 let audioCtx = new(window.AudioContext || window.webkitAudioContext)();
 let index = 0;
 //mainTune.reverse();
@@ -66,7 +110,7 @@ function playNote(frequency, duration) {
 }
 
 function playMelody() {
-  playNote( mainTune[index][0], 1000 * 256 / (mainTune[index][1] * tempo) );
+  playNote( mainTune[index][0] * 2, 1000 * 256 / (mainTune[index][1] * tempo) );
   if (index === mainTune.length - 1) { index = -1 }
   index++;
 }
